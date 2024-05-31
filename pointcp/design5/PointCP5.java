87f9aa075a93aca3dd4a23e7d69460fda1cc8627
AnthonyAR <147936783+AnthonyAR2005@users.noreply.github.com>
2024-05-31 16:55:50 -0400

    removed getDistance function from PointCP2 and PointCP3 class and added it in parent PointCP5 class

--- pointcp/design5/PointCP5.java
+++ pointcp/design5/PointCP5.java
@@ -8,7 +8,23 @@ public abstract class PointCP5 {
     public abstract void convertStorageToCartesian();
     public abstract void convertStorageToPolar();
 	
-	public abstract double getDistance(PointCP5 pointb);
+  /**
+   * Calculates the distance in between two points using the Pythagorean
+   * theorem  (C ^ 2 = A ^ 2 + B ^ 2). Not needed until E2.30.
+   *
+   * @param pointA The first point.
+   * @param pointB The second point.
+   * @return The distance between the two points.
+   */
+   public double getDistance(PointCP5 pointB){
+		// Obtain differences in X and Y, sign is not important as these values
+		// will be squared later.
+		double deltaX = getX() - pointB.getX();
+		double deltaY = getY() - pointB.getY();
+    
+		return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+   }
+  
 	public abstract PointCP5 rotatePoint(double rotation);
 	
     @Override
